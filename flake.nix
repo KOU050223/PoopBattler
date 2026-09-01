69d6e3296b91c00be169b67e04a6b635b177a9e8
KOU050223 <kouhei20050223@icloud.com>
2026-09-02 03:49:21 +0900

    chore: nixを使って環境を作成

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "poop-battler dev environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+
+  outputs =
+    { ... }@inputs:
+    let
+      inherit (inputs.nixpkgs) lib;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forEachSupportedSystem =
+        f: lib.genAttrs supportedSystems (system: f inputs.nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forEachSupportedSystem (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = [ pkgs.nodejs_24 ];
+        };
+      });
+
+      formatter = forEachSupportedSystem (pkgs: pkgs.nixfmt);
+    };
+}
